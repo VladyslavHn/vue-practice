6f7e59216712c9f05a00ea16757d8b23092d40eb
VladyslavHn <vlad.gnatok@gmail.com>
2024-09-12 01:33:45 +0200

    Rest-api

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,66 @@
 <template>
   <div class="container pt-1">
-    <div class="card">
-      <h2>News {{ now }}</h2>
+    <form class="card" @submit.prevent="cratePerson">
+      <h2>HTTP WORK</h2>
+
+      <div class="form-control">
+        <label for="name">Write your name</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Add person</button>
+
+    </form>
+
+    <AppPeopleList 
+    :people="people"
+    @load="loadPeople"
+    /> 
     </div>
-
-    <app-news 
-    v-for="(item, index) in news" 
-    :key="index"
-    :title="item.title"
-    :summary="item.summary">
-    </app-news>
-
-  </div>
 </template>
 
 <script>
-import AppNews from './components/AppNews.vue'
-import newsData from './News.json'
+import AppPeopleList from './components/AppPeopleList.vue'
+import axios from 'axios'
 
 export default {
-  data () {
+  data() {
     return {
-      now: new Date().toLocaleDateString(),
-      news: newsData.news
+      name: '',
+      people: []
     }
   },
-  components: {
-    'app-news': AppNews
-  }
+  methods: {
+    async cratePerson() {
+      const response = await fetch('https://vue-http-68bf0-default-rtdb.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json()
+      this.people.unshift({
+        firstName: this.name,
+        id: firebaseData.name
+      })
+      this.name = ''
+    },
+    async loadPeople() {
+      const { data } = await axios.get('https://vue-http-68bf0-default-rtdb.firebaseio.com/people.json')
+      const result = Object.keys(data).map(key => {
+        return {
+          id: key,
+          ...data[key]
+        }
+      })
+      this.people = result
+    }
+  },
+  components: { AppPeopleList }
 }
 </script>
 
---
+++ src/components/AppPeopleList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="people.length !== 0">
+        <div class="card" v-for="person in people" :key="person.id">
+            <h3>{{ person.firstName }}</h3>
+        </div>
+    </div>
+    <div class="card center" v-else>
+        <h3>We don't have people here yet! :(</h3>
+        <button class="btn" @click="$emit('load')">Load list</button>
+    </div> 
+</template>
+
+<script>
+export default {
+  emits: ['load'],
+  props: ['people', 'idx']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
