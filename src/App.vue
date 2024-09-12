fb42729a61f2cefa661ae51aefa81d2c7fd682e4
VladyslavHn <vlad.gnatok@gmail.com>
2024-09-12 12:42:11 +0200

    try/catch/errorHandler

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container pt-1">
+    <AppAlert :alert="alert" @close="alert = null"/>
     <form class="card" @submit.prevent="cratePerson">
       <h2>HTTP WORK</h2>
 
@@ -12,23 +13,34 @@
 
     </form>
 
+    <AppLoader v-if="loading"/>
+
     <AppPeopleList 
+    v-else
     :people="people"
     @load="loadPeople"
+    @remove="removePerson"
     /> 
     </div>
 </template>
 
 <script>
 import AppPeopleList from './components/AppPeopleList.vue'
+import AppAlert from './components/AppAlert.vue';
+import AppLoader from './components/AppLoader.vue';
 import axios from 'axios'
 
 export default {
   data() {
     return {
       name: '',
-      people: []
+      people: [],
+      alert: null,
+      loading: false
     }
+  },
+  mounted() {
+    this.loadPeople()
   },
   methods: {
     async cratePerson() {
@@ -41,7 +53,6 @@ export default {
           firstName: this.name
         })
       })
-
       const firebaseData = await response.json()
       this.people.unshift({
         firstName: this.name,
@@ -50,17 +61,48 @@ export default {
       this.name = ''
     },
     async loadPeople() {
-      const { data } = await axios.get('https://vue-http-68bf0-default-rtdb.firebaseio.com/people.json')
-      const result = Object.keys(data).map(key => {
-        return {
-          id: key,
-          ...data[key]
+      try {
+        this.loading = true
+        const { data } = await axios.get('https://vue-http-68bf0-default-rtdb.firebaseio.com/people.json')
+        if (!data) {
+          throw new Error('List is empty')
         }
-      })
-      this.people = result
+        this.people = Object.keys(data).map(key => {
+          return {
+            id: key,
+            ...data[key]
+          }
+        })
+        this.loading = false
+      } catch (e) {
+        this.alert = {
+          type: 'danger',
+          title: 'Error',
+          text: e.message
+        }
+        this.loading = false
+      }
+    },
+    async removePerson(id) {
+      try {
+        const name = this.people.find(person => person.id === id).firstName
+        await axios.delete(`https://vue-http-68bf0-default-rtdb.firebaseio.com/${id}.json`)
+        this.people = this.people.filter(person => person.id !== id)
+        this.alert = {
+          type: 'primary',
+          title: 'successful',
+          text: `User "${name}" was been deleted`
+        }
+      } catch (e) {
+        this.alert = {
+          type: 'danger',
+          title: 'Error',
+          text: e.message
+        }
+      }
     }
   },
-  components: { AppPeopleList }
+  components: { AppPeopleList, AppAlert, AppLoader }
 }
 </script>
 
